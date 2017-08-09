f20dafe2dbb65202d3d66836dcb2f1c6eb6a8f99
Guillaume Dumont <dumont.guillaume@gmail.com>
2017-08-09 19:46:51 -0400

    Disabled opencv static as it seems to work for me

--- ports/opencv/portfile.cmake
+++ ports/opencv/portfile.cmake
@@ -1,6 +1,6 @@
 if (VCPKG_LIBRARY_LINKAGE STREQUAL static)
-    message(STATUS "Warning: Static building not supported yet. Building dynamic.")
-    set(VCPKG_LIBRARY_LINKAGE dynamic)
+    # message(STATUS "Warning: Static building not supported yet. Building dynamic.")
+    # set(VCPKG_LIBRARY_LINKAGE dynamic)
 endif()
 include(vcpkg_common_functions)
 set(SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src/opencv-3.2.0)
